94196837a3c9af2eefb0a0f52b7b5ce8aa7aff29
nett89 <patryk.fajger.go@gmail.com>
2017-05-10 15:11:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 var appRoot = document.getElementById("app-root");
 appRoot.innerHTML = "<h2>Jak robić dobrą seo migrację</h2><p>Stosowanie szyfrowanego protokołu https umożliwia nie tylko zabezpieczenie przesyłanych informacji, dzięki wykorzystaniu https, strona jest bezpieczniejsza dla użytkownika, co  pozytywnie wpływa na ranking strony w naturalnych wynikach wyszukiwania Google. Dodatkowo, dla stron, które nie posiadają zabezpieczonego protokołu i jednocześnie pobierają dane użytkowników takie jak hasła do konta, w przeglądarkach internetowych będą wyświetlane komunikaty o niebezpiecznym połączeniu. Co z kolei spowoduje zmniejszenie ilości wizyt na danej stronie internetowej.</p>";
+
+var Data = function(){
+};
