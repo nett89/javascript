8f1c079382ef7f1cb9039428d32c30f9d260723c
nett89 <patryk.fajger.go@gmail.com>
2017-05-11 10:51:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ var Data1 = '<h1>' + 'Rekomendowane certyfikaty' + '</h1>' + '<p></p>' + 'Rekome
 function sayHello(name) {
 alert('Sposoby przenoszenia serwisów' + 'Jedną z najważniejszych kwestii związanych z przenoszeniem na wersję https jest dostępny Google Crawl Budget, od wielkości strony oraz ilości podstron indeksowanych (sprawdzanych przez Google bota) dziennie zależy czy przenosić całą stronę jednocześnie czy też przenoszenie na https rozbić na etapy, gdzie poszczególne części strony byłyby przenoszone w kolejnych etapach.')
 }
+
+function sayHello1(name) {
+document.getElementById("result").innerHTML = 'Jeżeli strona jest niewielka wówczas nie ma problemu z przeniesieniem strony 1 do 1, w takich przypadkach google bot w ciągu kilku tygodni powinien bez problemu zaindeksować nowe wersje. Przy stronach gdzie ilość podstron jest znacznie większa  w porównaniu do ilości stron sprawdzanych przez Google dziennie, powinno się przenosić serwis partiami, Ostatnią grupą serwisów są bardzo duże strony, z uwagi na ilość podstron w obrębie takich serwisów, sprawdzenie całości przez Google może zająć dużo czasu, w tym przypadku powinniśmy zastosować kanoniczne adresy url, umieszczone na obu wersjach i wskazujące wersję https jako wersję właściwą' + ' !';
+}
