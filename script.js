57dbfe195d58fe222e88e3ba4d2bbb0c3880868f
nett89 <patryk.fajger.go@gmail.com>
2017-05-11 10:00:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ appRoot.innerHTML = "<h2>Jak robić dobrą seo migrację</h2><p>Stosowanie szyfr
 var Data = function(){
   console.log("teksttekst");
 };
-var Data1 = "teksttekst1";
+var Data1 = '<h1>' + 'Rekomendowane certyfikaty' + '</h1>' + '< /br>' + '<p></p>' + 'Rekomendowany jest zakup komercyjnego certyfikatu SSL. Jeżeli mowa o kodowaniu  dostępnych jest kilka opcji, najlepiej jednak wybrać certyfikat SHA2 (SHA256) jest on bezpieczniejszy od SHA1, strony korzystające z SHA1 z uwagi na mniej bezpieczne szyfrowanie będą traktowane jako niezabezpieczone.';
