6dd35ef445fa4817612ccd7724cf2a74dc651672
nett89 <patryk.fajger.go@gmail.com>
2017-05-11 10:39:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ var Data1 = '<h1>' + 'Rekomendowane certyfikaty' + '</h1>' + '<p></p>' + 'Rekome
 
 
 function sayHello(name) {
-alert('<h1>' + 'Sposoby przenoszenia serwisów' + '</h1>')
+alert('Sposoby przenoszenia serwisów' + 'Jedną z najważniejszych kwestii związanych z przenoszeniem na wersję https jest dostępny Google Crawl Budget, od wielkości strony oraz ilości podstron indeksowanych (sprawdzanych przez Google bota) dziennie zależy czy przenosić całą stronę jednocześnie czy też przenoszenie na https rozbić na etapy, gdzie poszczególne części strony byłyby przenoszone w kolejnych etapach.')
 }
