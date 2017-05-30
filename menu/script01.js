5220e1d4ba3a6b872b0e1aedaa831bc3799c6955
nett89 <patryk.fajger.go@gmail.com>
2017-05-30 13:26:17 +0200

    Update script01.js

--- menu/script01.js
+++ menu/script01.js
@@ -1,25 +1,26 @@
-var appRoot = document.getElementById("app-root");
-appRoot.innerHTML = "<h2>Jak robić dobrą seo migrację</h2><p>Stosowanie szyfrowanego protokołu https umożliwia nie tylko zabezpieczenie przesyłanych informacji, dzięki wykorzystaniu https, strona jest bezpieczniejsza dla użytkownika, co  pozytywnie wpływa na ranking strony w naturalnych wynikach wyszukiwania Google. Dodatkowo, dla stron, które nie posiadają zabezpieczonego protokołu i jednocześnie pobierają dane użytkowników takie jak hasła do konta, w przeglądarkach internetowych będą wyświetlane komunikaty o niebezpiecznym połączeniu. Co z kolei spowoduje zmniejszenie ilości wizyt na danej stronie internetowej.</p>";
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
 
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
 
-var Data1 = '<h1>' + 'Rekomendowane certyfikaty' + '</h1>' + '<p></p>' + 'Rekomendowany jest zakup komercyjnego certyfikatu SSL. Jeżeli mowa o kodowaniu  dostępnych jest kilka opcji, najlepiej jednak wybrać certyfikat SHA2 (SHA256) jest on bezpieczniejszy od SHA1, strony korzystające z SHA1 z uwagi na mniej bezpieczne szyfrowanie będą traktowane jako niezabezpieczone.';
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
 
-
-function sayHello(name) {
-alert('Sposoby przenoszenia serwisów' + 'Jedną z najważniejszych kwestii związanych z przenoszeniem na wersję https jest dostępny Google Crawl Budget, od wielkości strony oraz ilości podstron indeksowanych (sprawdzanych przez Google bota) dziennie zależy czy przenosić całą stronę jednocześnie czy też przenoszenie na https rozbić na etapy, gdzie poszczególne części strony byłyby przenoszone w kolejnych etapach.')
-}
-
-function sayHello1(name) {
-document.getElementById("result").innerHTML = 'Jeżeli strona jest niewielka wówczas nie ma problemu z przeniesieniem strony 1 do 1, w takich przypadkach google bot w ciągu kilku tygodni powinien bez problemu zaindeksować nowe wersje. Przy stronach gdzie ilość podstron jest znacznie większa  w porównaniu do ilości stron sprawdzanych przez Google dziennie, powinno się przenosić serwis partiami, Ostatnią grupą serwisów są bardzo duże strony, z uwagi na ilość podstron w obrębie takich serwisów, sprawdzenie całości przez Google może zająć dużo czasu, w tym przypadku powinniśmy zastosować kanoniczne adresy url, umieszczone na obu wersjach i wskazujące wersję https jako wersję właściwą' + ' !';
-}
-
-function newData(parametr){
-  window.open().document.write('<p>' + 'Mamy przygotowana wersję https://' + '</p>' + 'Jednocześnie cały czas działa wersja http://');
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
 };
 
-var objekt01 = {
-    firstLine:"Na obu wersjach ustawiamy tag kanoniczny wskazujący wersję https://",
-    secLine:"Wdrażamy wersję https:// (obie wersje działają jednocześnie, na obu wersjach ustawiony jest tag kanoniczny wskazujący wersję https://)",
-    lastLine:"Kiedy wersja https:// zostanie zaindeksowana (zgoda działu SEO) ustawiamy przekierowanie 301 z wersji http:// na https://"
-};
- 
+
+$(document).ready(main);
