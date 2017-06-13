03b16add9c57da6246e232c6f6ed54d0d343b543
nett89 <patryk.fajger.go@gmail.com>
2017-06-13 14:07:38 +0200

    Create script2.js

---
+++ menu/strony/script2.js
@@ -0,0 +1,3 @@
+function sayHello(name) {
+alert('Sposoby przenoszenia serwisów' + 'Jedną z najważniejszych kwestii związanych z przenoszeniem na wersję https jest dostępny Google Crawl Budget, od wielkości strony oraz ilości podstron indeksowanych (sprawdzanych przez Google bota) dziennie zależy czy przenosić całą stronę jednocześnie czy też przenoszenie na https rozbić na etapy, gdzie poszczególne części strony byłyby przenoszone w kolejnych etapach.')
+}
