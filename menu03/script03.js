3b69f88234fbe8c66ac0a5853e13cb7a294b8fb9
nett89 <patryk.fajger.go@gmail.com>
2017-06-13 11:14:26 +0200

    Create script03.js

---
+++ menu03/script03.js
@@ -0,0 +1,67 @@
+<script>
+function myFunction() {
+    var new_div_menu = document.createElement("div");
+    new_div_menu.id = 'menu-rozwijane-id';
+    new_div_menu.className = 'menu-rozwijane';
+    new_div_menu.innerHTML = 'Menu generowane dynamicznie z zewnętrznego pliku po wczytaniu strony' + '<br>';
+for ( i=0; i<4; i++) {
+ 
+    var p1 = document.createElement('p');
+    new_div_menu.appendChild(p1);
+ 
+    if(i == 0) {
+    var a = document.createElement('a');
+    a.href =  'google.com'; // Insted of calling setAttribute 
+    a.innerHTML = "Link ";// <a>INNER_TEXT</a>
+    new_div_menu.appendChild(p1);
+    new_div_menu.appendChild(a); // Append the link to the div
+    
+    }
+    else if (i == 1) {
+    var a = document.createElement('a');
+    a.href =  'google.pl'; // Insted of calling setAttribute 
+    a.innerHTML = "Link2 ";// <a>INNER_TEXT</a>
+    new_div_menu.appendChild(p1);
+    new_div_menu.appendChild(a); // Append the link to the div
+    
+    }
+    else if (i == 2) {
+    var a = document.createElement('a');
+    a.href =  'google.pqql'; // Insted of calling setAttribute 
+    a.innerHTML = "Link3 ";// <a>INNER_TEXT</a>
+    new_div_menu.appendChild(p1);
+    new_div_menu.appendChild(a); // Append the link to the div
+    
+    }
+     else {
+    var a = document.createElement('a');
+    a.href =  'google.pwwwl'; // Insted of calling setAttribute 
+    a.innerHTML = "Link4 ";// <a>INNER_TEXT</a>
+    new_div_menu.appendChild(p1);
+    new_div_menu.appendChild(a); // Append the link to the div
+    
+    }
+ }
+    document.getElementById('menu-top').appendChild(new_div_menu);
+ 
+ 
+}
+ /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+    document.getElementById("menu-rozwijane-id").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("menu-rozwijane");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
